683d10b7510d0ec1d85435187be7300121208216
Aryan Srivastav <aryansrivastav2121@gmail.com>
2025-12-06 23:35:12 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js (Minimum Required Code)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // यह कॉन्फ़िगरेशन ऑब्जेक्ट है।
+  // अगर आपको कोई स्पेशल सेटिंग नहीं चाहिए, तो इसे खाली छोड़ सकते हैं।
+  reactStrictMode: true,
+};
+
+// *यह लाइन सबसे ज़रूरी है।*
+// यह Node.js को बताता है कि यह फ़ाइल क्या एक्सपोर्ट कर रही है।
+module.exports = nextConfig;
